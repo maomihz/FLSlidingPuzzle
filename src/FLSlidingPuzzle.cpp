c92b525cdc4b26a983c2a6f7bef1e49f8a022980
MaomiHz <maomihz@gmail.com>
2017-10-24 18:48:58 -0500

    Adapt font to all sizes of board

--- src/FLSlidingPuzzle.cpp
+++ src/FLSlidingPuzzle.cpp
@@ -17,14 +17,12 @@ GameBoard *gb;
 
 int main(int argc, char **argv) {
     win = new Fl_Window(0,0,800,600,"FL");
-    gb = new GameBoard(100,100,800,400);
-    // step = new Fl_Box(600,200,100,50,"bz");
+    gb = new GameBoard(100,100,800,400, 4);
 
 
 
     win->end();
     win->show(argc, argv);
     while (Fl::check()) {
-        // Fl::redraw();
     }
 }
--- src/GameBoard.cpp
+++ src/GameBoard.cpp
@@ -8,22 +8,28 @@ void GameBoard::draw() {
     // Call the draw function of the parent
     Fl_Box::draw();
     // Fill the background
-    fl_color(FL_WHITE);
+    fl_color(FL_BACKGROUND_COLOR);
     fl_rectf(x(),y(),w(),h());
 
     // Draw the numbers
     fl_color(FL_BLACK);
-    fl_font(FL_BOLD, 29);
-    for (int j = 0; j < 4; ++j) {
-        for (int i = 0; i < 4; ++i) {
+    int size = game.board().size();
+    int grid = h() / size;
+    fl_font(FL_BOLD, 36 - 2 * size);
+    for (int j = 0; j < size; ++j) {
+        for (int i = 0; i < size; ++i) {
             int num = game.board().at(i,j);
             const char* numstr = std::to_string(num).c_str();
-            if (num != 0)
-                fl_draw(numstr, x() + i * 100, y() + j * 100,
-                100, 100, FL_ALIGN_INSIDE);
-                // fl_rect(x() + j * 100, y() + i * 100)
+            if (num != 0) {
+                fl_draw(numstr,
+                    x() + i * grid, y() + j * grid,
+                    grid, grid, FL_ALIGN_INSIDE);
+                fl_rect(x() + i * grid, y() + j * grid, grid, grid);
+            }
         }
     }
+
+    // Draw Other stuff
     fl_font(fl_font(), 15);
     fl_draw("Steps",
         x() + 500, y() + 170,
