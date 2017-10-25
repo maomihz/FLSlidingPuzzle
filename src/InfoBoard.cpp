38ca6097a1b13b9c55ee63d0844ead618466d4d2
MaomiHz <maomihz@gmail.com>
2017-10-24 19:53:02 -0500

    Forget to add InfoBoard

---
+++ src/InfoBoard.cpp
@@ -0,0 +1,36 @@
+#include "InfoBoard.h"
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <iomanip>
+
+void InfoBoard::draw() {
+    // Call the draw function of the parent
+    Fl_Box::draw();
+    // Fill the background
+    fl_color(FL_BACKGROUND_COLOR);
+    fl_rectf(x(),y(),w(),h());
+
+    // Draw the text
+    fl_color(FL_BLACK);
+    fl_font(fl_font(), 15);
+    fl_draw("Steps", x(), y(),
+        100, 50, FL_ALIGN_INSIDE);
+    fl_draw("Time", x(), y() + 100,
+        100, 50, FL_ALIGN_INSIDE);
+
+    fl_draw(std::to_string(game->steps()).c_str(),
+        x(), y() + 50,
+        100, 50, FL_ALIGN_INSIDE);
+
+    // build the time
+    int duration = game->duration();
+    std::cout << duration << std::endl;
+    std::stringstream ss;
+    ss << std::setw(2) << std::setfill('0') << duration / 60;
+    ss << ":";
+    ss << std::setw(2) << std::setfill('0') << duration % 60;
+    fl_draw(ss.str().c_str(),
+        x(), y() + 150,
+        100, 50, FL_ALIGN_INSIDE);
+}
